ce984ab10ba33a4578122d3a77472aa291b8fea1
roeiPlayz <109908123+roeiPlayz@users.noreply.github.com>
2022-07-24 18:38:24 +0300

    Create pythonReview.py

---
+++ pythonReview.py
@@ -0,0 +1,17 @@
+def create_youtube_video(title,description):
+	video = {"Title": title, "Description": description, "Likes": 0, "Dislikes": 0, "Comments":{"Username": ""}}
+	return video
+
+def likes(video):
+	if "Likes" in video:
+		video["Liked"]+=1
+	return video
+
+def dislikes(video):
+	if "Dislikes" in video:
+		video["Dislikes"]+=1
+	return video
+
+def add_comment(video,Username,comment_text):
+	video["Comments"][Username] = comment_text
+	return video
